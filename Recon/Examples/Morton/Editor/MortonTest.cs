85a5dbc00b4b66168e5733d5fd8d677ad817ccd7
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-10-14 20:34:53 +0900

    モートンコード追加

---
+++ Recon/Examples/Morton/Editor/MortonTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using System.Text;
+
+namespace Reconnitioning {
+
+    public class MortonTest {
+
+    	[Test]
+    	public void EditorTest() {
+            Assert.AreEqual (0, MortonCode.Encode (0, 0, 0));
+
+            Assert.AreEqual (1, MortonCode.Encode (1, 0, 0));
+            Assert.AreEqual (8, MortonCode.Encode (2, 0, 0));
+
+            Assert.AreEqual (2, MortonCode.Encode (0, 1, 0));
+            Assert.AreEqual (16, MortonCode.Encode (0, 2, 0));
+
+            Assert.AreEqual (4, MortonCode.Encode (0, 0, 1));
+            Assert.AreEqual (32, MortonCode.Encode (0, 0, 2));
+
+            Assert.AreEqual (7, MortonCode.Encode (1, 1, 1));
+            Assert.AreEqual ((1 << 24), MortonCode.Encode (256, 0, 0));
+    	}
+    }
+}
---
+++ Recon/MortonCode.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Reconnitioning {
+    
+    public static class MortonCode {
+        const int BIT_COUNT = 8;
+        const int BIT_LIMIT = 1 << BIT_COUNT;
+        const int BIT_MASK = BIT_LIMIT - 1;
+        static readonly System.UInt32[] MORTON_X;
+        static readonly System.UInt32[] MORTON_Y;
+        static readonly System.UInt32[] MORTON_Z;
+
+        static MortonCode() {
+            MORTON_X = X2Morton (new System.UInt32[BIT_LIMIT], 0);
+            MORTON_Y = X2Morton (new System.UInt32[BIT_LIMIT], 1);
+            MORTON_Z = X2Morton (new System.UInt32[BIT_LIMIT], 2);
+        }
+
+        public static System.UInt64 Encode(int x, int y, int z) {
+            return Encode ((System.UInt32)x, (System.UInt32)y, (System.UInt32)z);
+        }
+        public static System.UInt64 Encode(System.UInt32 x, System.UInt32 y, System.UInt32 z) {
+            System.UInt64 m = 0;
+            for (var i = 2; i >= 0; i--) {
+                m = (m << (3 * BIT_COUNT))
+                    | MORTON_X[(x >> (BIT_COUNT * i)) & BIT_MASK]
+                    | MORTON_Y[(y >> (BIT_COUNT * i)) & BIT_MASK]
+                    | MORTON_Z[(z >> (BIT_COUNT * i)) & BIT_MASK];
+            }
+            return m;
+        }
+
+        static System.UInt32[] X2Morton(System.UInt32[] mortons, int offset) {
+            for (var i = 0; i < mortons.Length; i++)
+                mortons[i] = X2Morton (i) << offset;
+            return mortons;
+        }
+
+        static System.UInt32 X2Morton(int i) {
+            var m = 0;
+            for (var j = 0; j < BIT_COUNT; j++) {
+                var b = (i >> j) & 1;
+                m |= b << (3 * j);
+            }
+            return (System.UInt32)m;
+        }
+    }
+}
