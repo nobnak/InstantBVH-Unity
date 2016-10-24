39d2bdf5fa34f9ac90b85416f9ecd0091ad32d13
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-10-24 20:46:58 +0900

    短い型名に

--- MortonCode.cs
+++ MortonCode.cs
@@ -11,22 +11,22 @@ namespace Reconnitioning {
         public const int INT_MAX = (1 << 21) - 1;
         public const int INT_MIN = 0;
 
-        static readonly System.UInt32[] MORTON_X = X2Morton (new System.UInt32[STRIDE_LIMIT], 0);
-        static readonly System.UInt32[] MORTON_Y = X2Morton (new System.UInt32[STRIDE_LIMIT], 1);
-        static readonly System.UInt32[] MORTON_Z = X2Morton (new System.UInt32[STRIDE_LIMIT], 2);
+        static readonly uint[] MORTON_X = X2Morton (new uint[STRIDE_LIMIT], 0);
+        static readonly uint[] MORTON_Y = X2Morton (new uint[STRIDE_LIMIT], 1);
+        static readonly uint[] MORTON_Z = X2Morton (new uint[STRIDE_LIMIT], 2);
 
-        public static System.UInt64 Encode(int x, int y, int z) {
-            return Encode ((System.UInt32)x, (System.UInt32)y, (System.UInt32)z);
+        public static ulong Encode(int x, int y, int z) {
+            return Encode ((uint)x, (uint)y, (uint)z);
         }
-		public static System.UInt64 Encode(float x, float y, float z) {
-			return Encode ((System.UInt32)(x * INT_MAX), (System.UInt32)(y * INT_MAX), (System.UInt32)(z * INT_MAX));
+		public static ulong Encode(float x, float y, float z) {
+			return Encode ((uint)(x * INT_MAX), (uint)(y * INT_MAX), (uint)(z * INT_MAX));
 		}
-        public static System.UInt64 Encode(System.UInt32 x, System.UInt32 y, System.UInt32 z) {
+        public static ulong Encode(uint x, uint y, uint z) {
 			x = Clamp (x);
 			y = Clamp (y);
 			z = Clamp (z);
 
-            System.UInt64 m = 0;
+            ulong m = 0;
             for (var i = 2; i >= 0; i--) {
                 m = (m << (3 * STRIDE_BITS))
                     | MORTON_X[(x >> (STRIDE_BITS * i)) & STRIDE_MASK]
@@ -36,22 +36,22 @@ namespace Reconnitioning {
             return m;
         }
 
-		static System.UInt32 Clamp(System.UInt32 x) {
+		static uint Clamp(uint x) {
 			return (x < INT_MIN ? INT_MIN : (x <= INT_MAX ? x : INT_MAX));
 		}
-        static System.UInt32[] X2Morton(System.UInt32[] mortons, int offset) {
+        static uint[] X2Morton(uint[] mortons, int offset) {
             for (var i = 0; i < mortons.Length; i++)
                 mortons[i] = X2Morton (i) << offset;
             return mortons;
         }
 
-        static System.UInt32 X2Morton(int i) {
+        static uint X2Morton(int i) {
             var m = 0;
             for (var j = 0; j < STRIDE_BITS; j++) {
                 var b = (i >> j) & 1;
                 m |= b << (3 * j);
             }
-            return (System.UInt32)m;
+            return (uint)m;
         }
     }
 }
