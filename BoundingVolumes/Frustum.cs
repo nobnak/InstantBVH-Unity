fe9a6d48b65c46a53f1dff758c9fa31b4fe85e16
Nobuyuki Nakata <nobu.nakata@gmail.com>
2016-11-02 21:44:13 +0900

    backup

---
+++ BoundingVolumes/Frustum.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Recon.BoundingVolumes {
+        
+    public class Frustum {
+        public Vector3 head;
+        public Vector3 farBottomLeft;
+        public Quaternion axis;
+
+        public Frustum(Vector3 head, Vector3 farBottomLeft, Quaternion axis) {
+            this.head = head;
+            this.farBottomLeft = farBottomLeft;
+            this.axis = axis;
+        }
+
+        public float FoV() {
+            return 2f * Mathf.Atan2 (farBottomLeft.y, farBottomLeft.z) * Mathf.Rad2Deg;
+        }
+        public float Aspect() {
+            return farBottomLeft.x / farBottomLeft.y;
+        }
+        public float MaxRange() {
+            return farBottomLeft.z;
+        }
+        public Frustum DrawGizmos() {
+            Gizmos.matrix = Matrix4x4.TRS (head, axis, Vector3.one);
+            Gizmos.DrawLine (Vector3.zero, farBottomLeft);
+            Gizmos.DrawFrustum (Vector3.zero, FoV (), MaxRange (), 1e-6f, Aspect ());
+            return this;
+        }
+    }
+}
---
+++ BoundingVolumes/OBB.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Recon.BoundingVolumes {
+
+    public class OBB {
+        public Vector3 center;
+        public Vector3 size;
+        public Quaternion axis;
+
+        public OBB(Vector3 center, Vector3 size, Quaternion axis) {
+            this.center = center;
+            this.size = size;
+            this.axis = axis;
+        }
+
+        public OBB DrawGizmos() {
+            Gizmos.matrix = Matrix4x4.TRS (center, axis, size);
+            Gizmos.DrawWireCube (Vector3.zero, Vector3.one);
+            return this;
+        }
+
+        #region Static
+        public static OBB Create(Transform tr, Bounds localBounds) {
+            return new OBB (
+                tr.TransformPoint (localBounds.center),
+                Vector3.Scale (tr.localScale, localBounds.size),
+                tr.rotation);
+        }
+        #endregion
+    }
+}
