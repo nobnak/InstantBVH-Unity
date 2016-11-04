1877498a155e7fa9522d0c3c48497c2bd680bcb1
Naka Nobuyuki <nobu.nakata@gmail.com>
2016-11-04 11:45:53 +0900

    backup

--- BoundingVolumes/Intersection.cs
+++ BoundingVolumes/Intersection.cs
@@ -6,7 +6,9 @@ namespace Recon.BoundingVolumes {
     public static class Intersection {
 
         public static bool Intersect(IConvexPolyhedron a, IConvexPolyhedron b) {
-            
+			foreach (var ae in a.Edges())
+				foreach (var be in b.Edges())
+					
         }
     }
 }
