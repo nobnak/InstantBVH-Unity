ef1aff877acb67badbc620b8013611c4a52aefcb
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-10-11 21:03:04 +0900

    SAH 版 BVH を書いてみた

--- SpacePartition/BinnedSAH.cs
+++ SpacePartition/BinnedSAH.cs
@@ -1,44 +1,146 @@
-﻿using Gist.BoundingVolume;
+﻿using Gist;
+using Gist.Scoped;
+using Gist.BoundingVolume;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Recon.SpacePartition {
 
-    public class BinnedSAH {
+    public class BinnedSAH : System.IDisposable {
+        public const float SMALLER_THAN_ONE = 0.999f;
+        public const int DEFAULT_K = 8;
 
-        protected int k = 0;
-        protected AABB[] bb;
+        protected int binCount = 0;
+        protected AABB[] bbs;
         protected List<int>[] bins;
 
         protected float[] lefts;
         protected float[] rights;
 
-        public void Reset(int binCount) {
-            ResetBB(binCount);
-            ResetBins(binCount);
-            ResetHeuristic(binCount);
+        protected bool cleared;
+
+        protected IMemoryPool<AABB> aabbPool;
+        protected List<AABB> primitiveBounds;
+
+        public BinnedSAH(int binCount, IMemoryPool<AABB> aabbPool = null) {
+            this.aabbPool = (aabbPool == null ? AABB.CreateAABBPool() : aabbPool);
+            Reset(binCount);
+        }
+        public BinnedSAH(IMemoryPool<AABB> aabbPool = null) : this(DEFAULT_K, aabbPool) { }
+
+        public void Build(IList<Bounds> bounds, IList<int> indices, int offset, int length, out int countFromLeft) {
+            Clear();
+            cleared = false;
+
+            var world = aabbPool.New();
+            using (new ScopedPlug<IList<AABB>>(primitiveBounds, d => Free(d, aabbPool))) {
+                for (var i = 0; i < length; i++) {
+                    var j = indices[i + offset];
+                    var ob = aabbPool.New();
+                    ob.Set(bounds[j]);
+                    world.Encapsulate(ob);
+                    primitiveBounds.Add(ob);
+                }
+
+                var size = world.Size;
+                var longestAxis = (size.x > size.z ? (size.x > size.y ? 0 : 1) : (size.y > size.z ? 1 : 2));
+                var longest = size[longestAxis];
+                var worldMin = world.Min[longestAxis];
+
+                var invLongest = 1f / longest;
+                for (var i = 0; i < length; i++) {
+                    var j = indices[i + offset];
+                    var ob = primitiveBounds[i];
+                    var c = ob.Center;
+                    var k = (int)(binCount * (c[longestAxis] - worldMin) * invLongest * SMALLER_THAN_ONE);
+                    bins[k].Add(j);
+                    bbs[k].Encapsulate(ob);
+                }
+
+                var leftCount = 0;
+                var leftBounds = aabbPool.New();
+                for (var i = 0; i < lefts.Length; i++) {
+                    leftCount += bins[i].Count;
+                    leftBounds.Encapsulate(bbs[i]);
+                    lefts[i] = leftCount * leftBounds.SurfaceArea;
+                }
+
+                var rightCount = 0;
+                var rightBounds = aabbPool.New();
+                for (var i = 0; i < rights.Length; i++) {
+                    var j = binCount - i - 1;
+                    rightCount += bins[j].Count;
+                    rightBounds.Encapsulate(bbs[j]);
+                    rights[j] = rightCount * rightBounds.SurfaceArea;
+                }
+
+                var bestIndex = -1;
+                var bestCost = float.MaxValue;
+                for (var i = 1; i < binCount; i++) {
+                    var cost = lefts[i - 1] + rights[i];
+                    if (cost < bestCost) {
+                        bestCost = cost;
+                        bestIndex = i;
+                    }
+                }
+
+                countFromLeft = 0;
+                for (var i = 0; i < bestIndex; i++)
+                    countFromLeft += bins[i].Count;
+
+                var offsetOfIndices = offset;
+                foreach (var bin in bins)
+                    foreach (var i in bin)
+                        indices[offsetOfIndices++] = i;
+
+                Debug.LogFormat("Best SAH : Best index={0} cost={1}, Split index={2}", bestIndex, bestCost, countFromLeft);
+            }
         }
 
-        private void ResetHeuristic(int binCount) {
-            System.Array.Resize(ref lefts, binCount);
-            System.Array.Resize(ref rights, binCount);
+        public void Reset(int nextBinCount) {
+            if (binCount == nextBinCount)
+                return;
+            var prevBinCount = binCount;
+            binCount = nextBinCount;
+            cleared = false;
+
+            Free(bbs, aabbPool);
+            System.Array.Resize(ref bbs, nextBinCount);
+            for (var i = 0; i < bbs.Length; i++)
+                bbs[i] = aabbPool.New();
+
+            System.Array.Resize(ref bins, nextBinCount);
+            for (var i = prevBinCount; i < nextBinCount; i++)
+                bins[i] = new List<int>();
+
+            System.Array.Resize(ref lefts, nextBinCount);
+            System.Array.Resize(ref rights, nextBinCount);
+        }
+
+        public void Clear() {
+            if (cleared)
+                return;
+            cleared = true;
+
+            for (var i = 0; i < binCount; i++) {
+                bins[i].Clear();
+                bbs[i].Clear();
+            }
+
             System.Array.Clear(lefts, 0, lefts.Length);
             System.Array.Clear(rights, 0, rights.Length);
         }
-        private void ResetBB(int binCount) {
-            var prevBBCount = k;
-            System.Array.Resize(ref bb, binCount);
-            for (var i = prevBBCount; i < binCount; i++)
-                bb[i] = new AABB();
+
+        public void Dispose() {
+            Free(bbs, aabbPool);
         }
-        private void ResetBins(int binCount) {
-            var prev = k;
-            System.Array.Resize(ref bins, binCount);
-            for (var i = 0; i < prev; i++)
-                bins[i].Clear();
-            for (var i = prev; i < binCount; i++)
-                bins[i] = new List<int>();
+
+        public static void Free<T>(IList<T> data, IMemoryPool<T> pool) {
+            foreach (var d in data)
+                pool.Free(d);
+            data.Clear();
         }
+
     }
 }
--- SpacePartition/BinnedSAHBVHController.cs
+++ SpacePartition/BinnedSAHBVHController.cs
@@ -6,20 +6,57 @@ using Recon.Treap;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using Gist.BoundingVolume;
 
 namespace Recon.SpacePartition {
 
     public class BinnedSAHBVHController<Value> : BaseBVHController<Value> where Value : class {
         public const int K = 8;
 
+        protected IMemoryPool<AABB> pool;
+        protected List<int> indices;
+        protected BinnedSAH sah;
+
+        public BinnedSAHBVHController() {
+            this.pool = AABB.CreateAABBPool();
+            this.indices = new List<int>();
+            this.sah = new BinnedSAH(pool);
+        }
+
+        public override BaseBVHController<Value> Clear() {
+            base.Clear();
+            indices.Clear();
+            return this;
+        }
+
         public override BaseBVHController<Value> Build(IList<Bounds> Bous, IList<Value> Vals) {
             Clear ();
 
+            sah.Clear();
+            for (var i = 0; i < Bous.Count; i++)
+                indices.Add(i);
 
+            _root = Build(Bous, indices, 0, indices.Count, sah, _pool);
 
             return this;
         }
 
+        public static BVH<Value> Build(IList<Bounds> bounds, IList<int> indices, int offset, int length, 
+            BinnedSAH sah, IMemoryPool<BVH<Value>> alloc) {
+            if (length <= 0)
+                return null;
 
+            if (length <= 2)
+                return alloc.New().Reset(offset, length);
+
+            int countFromLeft;
+            sah.Build(bounds, indices, offset, length, out countFromLeft);
+
+            var l = Build(bounds, indices, offset, countFromLeft, sah, alloc);
+            var r = Build(bounds, indices, offset + countFromLeft, length - countFromLeft, sah, alloc);
+            if (l != null ^ r != null)
+                return (l != null) ? l : r;
+            return alloc.New().Reset(offset, length).SetChildren(l, r);
+        }
     }
 }
