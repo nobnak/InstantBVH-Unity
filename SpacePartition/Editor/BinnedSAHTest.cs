2425baa5afde963efc1476078bd9919b8fb6f3b4
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-10-12 13:12:25 +0900

    Test 項目を用意

---
+++ SpacePartition/Editor/BinnedSAHTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using Gist.BoundingVolume;
+
+public class BinnedSAHTest {
+
+	[Test]
+	public void BinnedSAHTestSimplePasses() {
+
+        var bounds = new List<AABB>();
+        var indices = new List<int>();
+
+
+	}
+}
